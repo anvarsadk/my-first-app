aed5ef9006d8258043903f3c85aaa7b51c04e533
Anvar Sadath Kalanchira <akalanch@cisco.com>
2023-01-04 16:44:07 -0500

    assignment 2: directives ngIf ngFor ngClass ngStyle

---
+++ src/app/display-details/display-details.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-details',
+  templateUrl: './display-details.component.html',
+  styleUrls: ['./display-details.component.css']
+})
+export class DisplayDetailsComponent implements OnInit {
+  showContent = true;
+  clickLog = [];
+  // clickCount = 0;
+  // bgColor = 'white';
+  // showColor = false;
+
+  toggleContent() {
+    this.showContent = !(this.showContent);
+    this.clickLog.push(this.clickLog.length+1);
+    /*if(this.clickCount==5) {
+      this.bgColor = 'blue';
+      this.showColor = true;
+    }*/
+  }
+
+  showBlueBackground(c){
+    return (c>=5)? 'blue': 'white';
+  }
+
+  showWhiteColor(c){
+    return c>=5;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
